adfee70f3be226870f9ca1e2a444050712dbeb41
YiLinLiu123 <liuyilin.liu@gmail.com>
2019-11-04 15:25:14 -0800

    finished Version

--- programmingAssignment2.cpp
+++ programmingAssignment2.cpp
@@ -9,8 +9,9 @@ using namespace std;
 using namespace Eigen; 
 
 // constants
-const double H = 0.05;
+const double H = 0.025; //step value in space, I used h cause thats what we use in math classes. 
 const double U = 2.0; 
+
 
 
 void displayVector(RowVectorXd& a, int size) {
@@ -32,6 +33,7 @@ void initalizeArray(RowVectorXd& a, int size) {
 		a(i) = (-cos(M_PI * (x_end)) + cos(M_PI * x_start)) / (M_PI * H);
 	}
 }
+
 //index shifting macro as recommended by Carl
 int indexShift(int N, int index) {
 	int newIndex = (index + N) % N;
@@ -39,7 +41,6 @@ int indexShift(int N, int index) {
 }
 
 // UW2 flux integral evaulation
-
 void UW2_Flux_Integral(RowVectorXd& data, RowVectorXd&  result, int size) {
 	for (int i = 0; i < size; i++) {
 		int indexI = indexShift(size,i);
@@ -58,12 +59,11 @@ void UW2_Flux_Integral(RowVectorXd& data, RowVectorXd&  result, int size) {
 	}
 }
 
-// C2 flux integral evaulation
-
+// C2 flux integral evaulatio
 void C2_Flux_Integral(RowVectorXd& data, RowVectorXd& result, int size) {
 	for (int i = 0; i < size; i++) {
-		int indexIM1 = indexShift(size, i - 1);
-		int indexIP1 = indexShift(size, i +1);
+		int indexIM1 = indexShift(size, i-1);
+		int indexIP1 = indexShift(size, i+1);
 
 		double tIM1 = data(indexIM1);
 		double tIP1 = data(indexIP1);
@@ -74,7 +74,6 @@ void C2_Flux_Integral(RowVectorXd& data, RowVectorXd& result, int size) {
 }
 
 //UWB3 flux integral evaulation
-// note i am calculating U*(Ti+1/2- Ti-1/2)/(deltaX)
 void UWB3_Flux_Integral(RowVectorXd& data, RowVectorXd& result,  int size) {
 	for (int i = 0; i < size; i++) {
 		int indexI = indexShift(size, i);
@@ -105,9 +104,10 @@ double flux_L2_Norm(RowVectorXd& CV, int size, RowVectorXd & errorVector, RowVec
 	double cumError = 0; 
 	for (int i = 0; i < size; i++) {
 		double x = (i + 1.0 / 2.0) * H-1;
+		//double x = (i) * H - 1;
 		double correct = correctValueFunction(x);
 		double guess = CV(i);
-		double error = abs(guess - correct);
+		double error = guess - correct;
 		errorVector(i) = error;
 		correctVector(i) = correct; 
 		//printf("error: %f \n", error);
@@ -136,26 +136,13 @@ void RK2(RowVectorXd& data, RowVectorXd& updatedResult, void (*flux_Integral)(Ro
 	updatedResult = data + timeStep * (fluxIntegral1);
 }
 
-void RK2_Validation(RowVectorXd& data, RowVectorXd& updatedResult, void (*flux_Integral)(RowVectorXd& data, RowVectorXd& result, int size, double time), int size, double timeStep, double currentTime)
-{
-	// calculate the space flux
-	Eigen::RowVectorXd fluxIntegral(size);
-	Eigen::RowVectorXd fluxIntegral1(size);
-	flux_Integral(data, fluxIntegral, size, currentTime);
 
-	// constants
-	RowVectorXd w_1(size);
-	w_1 = data + timeStep / 2.0 * fluxIntegral;
-	flux_Integral(w_1, fluxIntegral1, size, currentTime);
-	updatedResult = data + timeStep * (fluxIntegral1);
-
-}
 
 
 void RK4(RowVectorXd& data, RowVectorXd& updatedResult, void flux_Integral(RowVectorXd& data, RowVectorXd& result, int size), int size, double timeStep)
 {
 	// vectors
-	Eigen::RowVectorXd fluxIntegral (size);
+	Eigen::RowVectorXd fluxIntegral(size);
 	Eigen::RowVectorXd w_1(size);
 	Eigen::RowVectorXd fluxIntegral1(size);
 	Eigen::RowVectorXd w_2(size);
@@ -165,15 +152,51 @@ void RK4(RowVectorXd& data, RowVectorXd& updatedResult, void flux_Integral(RowVe
 
 	// steps
 	flux_Integral(data, fluxIntegral, size);
-	w_1 = data + timeStep/2 * fluxIntegral;
+	w_1 = data + timeStep / 2.0 * fluxIntegral;
 
 	flux_Integral(w_1, fluxIntegral1, size);
-	w_2 = data + timeStep / 2 * fluxIntegral1;
+	w_2 = data + timeStep / 2.0 * fluxIntegral1;
+
 
 	flux_Integral(w_2, fluxIntegral2, size);
-	w_3 = data + timeStep / 2 * fluxIntegral2;
+	w_3 = data + timeStep * fluxIntegral2;
 
 	flux_Integral(w_3, fluxIntegral3, size);
+	updatedResult = data + timeStep / 6.0 * (fluxIntegral + 2.0 * fluxIntegral1 + 2.0 * fluxIntegral2 + fluxIntegral3);
+
+}
+
+// made the same thing except has a lot of print statements for debugging. 
+void RK4Debug(RowVectorXd& data, RowVectorXd& updatedResult, void flux_Integral(RowVectorXd& data, RowVectorXd& result, int size), int size, double timeStep)
+{
+	// vectors
+	Eigen::RowVectorXd fluxIntegral(size);
+	Eigen::RowVectorXd w_1(size);
+	Eigen::RowVectorXd fluxIntegral1(size);
+	Eigen::RowVectorXd w_2(size);
+	Eigen::RowVectorXd fluxIntegral2(size);
+	Eigen::RowVectorXd w_3(size);
+	Eigen::RowVectorXd fluxIntegral3(size);
+
+	// steps
+	flux_Integral(data, fluxIntegral, size);
+	cout << "flux integral: " <<fluxIntegral << endl;
+	w_1 = data + timeStep / 2.0 * fluxIntegral;
+	cout << "w1: " << endl << w_1 << endl;
+
+	flux_Integral(w_1, fluxIntegral1, size);
+	cout << "flux integral1: " << endl << fluxIntegral1 << endl;
+	w_2 = data + timeStep / 2.0 * fluxIntegral1;
+	cout << "w2: " << endl << w_2 << endl;
+
+
+	flux_Integral(w_2, fluxIntegral2, size);
+	cout << "flux integral2: " << endl << fluxIntegral2 << endl;
+	w_3 = data + timeStep * fluxIntegral2;
+	cout << "w3: " << endl << w_3 << endl;
+
+	flux_Integral(w_3, fluxIntegral3, size);
+	cout << "flux integral3: " << endl << fluxIntegral3 << endl;
 	updatedResult = data + timeStep / 6.0 * (fluxIntegral + 2.0 * fluxIntegral1 + 2.0 * fluxIntegral2 + fluxIntegral3);
 
 }
@@ -181,6 +204,7 @@ void RK4(RowVectorXd& data, RowVectorXd& updatedResult, void flux_Integral(RowVe
 double partOneRightValue(double x) {
 	return -1.0*U * M_PI * cos(M_PI * x);
 }
+
 void partOne()
 {
 	const int numCells = 2.0 / H;
@@ -189,7 +213,6 @@ void partOne()
 	RowVectorXd CV_UW2_Array(numCells);
 	RowVectorXd CV_C2_Array(numCells);
 	RowVectorXd CV_UWB3_Array(numCells);
-
 
 	RowVectorXd errorVector(numCells);
 	RowVectorXd correctVector(numCells);
@@ -212,52 +235,26 @@ void partOne()
 	cout << CV_C2_Array << endl;
 	printf("L2 Norm for C2 where N = %d: %f \n", numCells, flux_L2_Norm(CV_C2_Array, numCells, errorVector, correctVector, partOneRightValue));
 	displayVector(errorVector, numCells);
-
-	// cv for UWB3
-	UWB3_Flux_Integral(cellArray, CV_UWB3_Array, numCells);
-	printf("CV_UWB3_Values: \n");
-	cout << CV_UWB3_Array << endl;
-	printf("L2 Norm for UWB3 where N = %d: %f \n", numCells, flux_L2_Norm(CV_UWB3_Array, numCells, errorVector, correctVector, partOneRightValue));
-	displayVector(errorVector, numCells);
-	printf("CorrectVector \n");
-	cout << correctVector << endl;
 }
 
 
-void fakeFluxIntegral(RowVectorXd& data, RowVectorXd& result, int size, double time) {
+void exactFluxIntegral(RowVectorXd& data, RowVectorXd& result, int size, double x) {
 	// we know the exact solution
 	// solve over 0 to .5
-	result(0) = U/H*( sin(M_PI*(H+U*time))- sin(M_PI * ( U * time)));
-
-	// try random one
-	//dT/dt = xt
-	//result(0) =time  ;
-}
-
-void partTwoRK2() {
-
-	// "solving" du/dt= sin(piX)
-	RowVectorXd cellArray(1); // our solution vector of one
-	RowVectorXd nextTimeStep(1);
-	double timeStep = 0.025;
-	cellArray(0) = sin(M_PI*H/2.0); 
-	RK2_Validation(cellArray, nextTimeStep, fakeFluxIntegral, 1,timeStep,0);
-	cout << cellArray << endl;
-	cout << nextTimeStep << endl;
-
-	RK2_Validation(nextTimeStep, nextTimeStep, fakeFluxIntegral, 1,timeStep,timeStep);
-	cout << nextTimeStep << endl;
+	result(0) = -U*M_PI*cos(M_PI*x);
 
 }
+
 
 double partThreeRightValue(double x) {
 	return  sin(M_PI * x);
 }
 void partThree_RK2_And_UW2() {
 	double targetTime = 2.0; 
-	//double timeStep = 0.05/(0.2/H)*0.8; 
-	double timeStep = 0.006*0.8; // for H = 0.025
-	int steps = 2.0 / timeStep;
+	double timeStep = 0.05/(0.2/H)*0.8; 
+	//double timeStep = 0.006*0.8; // for H = 0.025
+	int steps = ceil(2.0 / timeStep);
+	timeStep = 2.0 / steps;
 	const int numCells = 2.0 / H;
 	double time = 0; 
 	
@@ -266,13 +263,15 @@ void partThree_RK2_And_UW2() {
 	RowVectorXd error(numCells);
 
 	initalizeArray(cellArray,numCells);
-	initalizeArray(correct, numCells);
+	cout << "time step: " << timeStep << endl;
+	cout << "dx: " << H << endl; 
 	cout << "initial values: " <<cellArray << endl;
 
 	for (int i = 0; i < steps; i++) {
 		time = time +timeStep;
 		RK2(cellArray, cellArray, UW2_Flux_Integral, numCells,timeStep);
 	}
+	cout << "final time: " << time << endl;
 	cout << "final iteration: " << endl;
 	displayVector(cellArray, numCells);
 	cout <<"error norm : "<<flux_L2_Norm(cellArray, numCells, error, correct, partThreeRightValue) << endl;
@@ -284,10 +283,14 @@ void partThree_RK2_And_UW2() {
 }
 
 void partThree_RK4_And_C2() {
-	double targetTime = 2.0;
-	//double timeStep = 0.05/(0.2/H)*0.8; 
-	double timeStep = 0.0002*.8; // for H = 0.25
-	int steps = 2.0/timeStep;
+    double timeStep = 0.28/(.2/H)*0.8; // for C2
+	//double timeStep = 0.05 / (0.2 / H) * 0.8; // for UW2
+	// double timeStep = 0.16 / (0.2 / H) * 0.8; // for UWB3
+	//double constant = 0.7071067;
+	//double timeStep = 0.01*constant*constant;
+	double CFL = timeStep / H * U;
+	int steps = ceil(2.0/timeStep);// happens that we need to ceil it
+	timeStep = 2.0 / steps; 
 	const int numCells = 2.0 / H;
 	double time = 0;
 
@@ -296,13 +299,16 @@ void partThree_RK4_And_C2() {
 	RowVectorXd error(numCells);
 
 	initalizeArray(cellArray, numCells);
-	initalizeArray(correct, numCells);
+	cout << "time step: " << timeStep << endl;
+	cout << "dx: " << H << endl;
+	cout << "CFL: " << CFL << endl;
 	cout << "initial values: " << cellArray << endl;
 
-	for (int i = 0; i < steps; i++) {
+	for (int i = 0; i <steps; i++) {
 		time = time + timeStep;
 		RK4(cellArray, cellArray, C2_Flux_Integral, numCells, timeStep);
 	}
+	cout << "final time: " << time << endl;
 	cout << "final iteration: " << endl;
 	displayVector(cellArray, numCells);
 	cout << "error norm : " << flux_L2_Norm(cellArray, numCells, error, correct, partThreeRightValue) << endl;
@@ -315,9 +321,8 @@ void partThree_RK4_And_C2() {
 
 int main()
 {
-	//partOne();
-	//partTwoRK2();
+	partOne();
 	partThree_RK2_And_UW2();
-	//partThree_RK4_And_C2();
+	partThree_RK4_And_C2();
 
 }
